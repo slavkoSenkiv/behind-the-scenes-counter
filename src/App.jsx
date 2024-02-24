df4ce6d16fc5472ec93187b66f9cca2854749016
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-02-24 21:07:52 +0100

    lear downsides of key as index and how to fix it

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       <main>
         <ConfigureCounter onSet={handleSetCount} />
         <Counter initialCount={chosenCount} />
+        <Counter initialCount={0} />
       </main>
     </>
   );
